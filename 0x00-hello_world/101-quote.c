f97c5b052783b503cbf9ead0527ad29c4d4e61d9
Nathanael-Nii <nathanaelallotey@gmail.com>
2023-03-20 09:00:39 -0700

    operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
- * main - A c program that prints line 
+ * main - c program that prints
  *
  * Return: always 1
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (0);
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
