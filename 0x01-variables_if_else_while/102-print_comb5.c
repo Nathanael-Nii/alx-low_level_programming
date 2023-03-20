48ae84edd2154387ee9dabb9da93213b0232a40a
Nathanael-Nii <nathanaelallotey@gmail.com>
2023-03-20 08:36:10 -0700

    software

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - main fucntion
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0 ; a <= 98 ; a++)
+	{
+		for (b = a + 1 ; b <= 99 ; b++)
+		{
+			if (a == 98 && b == 99)
+			{
+				putchar((a / 10) + '0');
+				putchar((a % 10) + '0');
+				putchar(' ');
+				putchar((b / 10) + '0');
+				putchar((b / 10) + '0');
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
