8ce7457c4a61756c2d43ccfc4d0af4a9f2c2260e
Nathanael-Nii <nathanaelallotey@gmail.com>
2023-03-30 07:00:56 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncat - concatenates two strings
+ *
+ * @src: source of strings
+ * @dest: destination of string
+ * @n: int check
+ *
+ * Return: always 0
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int a;
+	int b;
+
+	for (a = 0; dest[a] != '\0'; a++)
+	{
+		continue;
+	}
+	for (b = 0; src[b] != '\0' && b < n; b++)
+	{
+		dest[a + b] = src[b];
+	}
+	dest[a + b] = '\0';
+	return (0);
+}
