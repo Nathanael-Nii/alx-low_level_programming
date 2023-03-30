b06ab6947e9cf697fe81639e3ef994798887628f
Nathanael-Nii <nathanaelallotey@gmail.com>
2023-03-30 06:54:37 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,15 +10,19 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int destlen = 0;
-	int srclen = 0;
-	int a;
+	char *s = dest;
 
-	for (a = 0 ; dest[a] != '\0' ; a++)
-		destlen++; |
-			for (a = 0 ; src[a] != '\0' ; a++)
-				srclen++; |
-					for (a = 0 ; a <= srclen ; a++)
-						dest[destlen + a] = src[a];
-	return (dest);
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (s);
 }
