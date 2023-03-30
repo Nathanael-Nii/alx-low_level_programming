e3ed24687a3f581e0816a595ec1d8b0a435f21a1
Nathanael-Nii <nathanaelallotey@gmail.com>
2023-03-30 06:37:50 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates 2 string
+ * @dest: destination string
+ * @src: source string
+ * Return: always 0
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *s = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (0);
+}
